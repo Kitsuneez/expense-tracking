4b69975acac375ae8060e30fef714802e2955f6f
Kitsuneez <106949197+Kitsuneez@users.noreply.github.com>
2024-08-16 19:25:13 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# expense-tracking
+# Expense Tracking
+
+This program scans the gmail and captures the different labels depending on what category the email is labeled for.
+Emails that can be scanned and automated are:
+
+- PayLah!
+- ibanking.alert@DBS
+
+However, this program is not able to detect incoming payments from others as it is still an ongoing project.
+It makes use of different APIs such as google and AWS DynamoDB.
+
+I will not be going in depth on how to set up the google account and AWS DynamoDB as it is only for personal purposes and not for commercial use.
---
+++ index.js
@@ -0,0 +1,115 @@
+const fs = require('fs').promises;
+const path = require('path');
+const process = require('process');
+const {authenticate} = require('@google-cloud/local-auth');
+const {google} = require('googleapis');
+const { param } = require('express/lib/request');
+
+// If modifying these scopes, delete token.json.
+const SCOPES = ['https://www.googleapis.com/auth/gmail.readonly'];
+// The file token.json stores the user's access and refresh tokens, and is
+// created automatically when the authorization flow completes for the first
+// time.
+const TOKEN_PATH = path.join(process.cwd(), 'token.json');
+const CREDENTIALS_PATH = path.join(process.cwd(), 'credentials.json');
+
+/**
+ * Reads previously authorized credentials from the save file.
+ *
+ * @return {Promise<OAuth2Client|null>}
+ */
+async function loadSavedCredentialsIfExist() {
+  try {
+    const content = await fs.readFile(TOKEN_PATH);
+    const credentials = JSON.parse(content);
+    return google.auth.fromJSON(credentials);
+  } catch (err) {
+    return null;
+  }
+}
+
+/**
+ * Serializes credentials to a file compatible with GoogleAuth.fromJSON.
+ *
+ * @param {OAuth2Client} client
+ * @return {Promise<void>}
+ */
+async function saveCredentials(client) {
+  const content = await fs.readFile(CREDENTIALS_PATH);
+  const keys = JSON.parse(content);
+  const key = keys.installed || keys.web;
+  const payload = JSON.stringify({
+    type: 'authorized_user',
+    client_id: key.client_id,
+    client_secret: key.client_secret,
+    refresh_token: client.credentials.refresh_token,
+  });
+  await fs.writeFile(TOKEN_PATH, payload);
+}
+
+/**
+ * Load or request or authorization to call APIs.
+ *
+ */
+async function authorize() {
+  let client = await loadSavedCredentialsIfExist();
+  if (client) {
+    return client;
+  }
+  client = await authenticate({
+    scopes: SCOPES,
+    keyfilePath: CREDENTIALS_PATH,
+  });
+  if (client.credentials) {
+    await saveCredentials(client);
+  }
+  return client;
+}
+
+/**
+ * Lists the labels in the user's account.
+ *
+ * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
+ */
+async function listLabels(auth) {
+  const gmail = google.gmail({version: 'v1', auth});
+  const res = await gmail.users.labels.list({
+    userId: 'me',
+  });
+  const labels = res.data.labels;
+  if (!labels || labels.length === 0) {
+    console.log('No labels found.');
+    return;
+  }
+  console.log('Labels:');
+  labels.forEach((label) => {
+    console.log(`- ${label.name}`);
+  });
+}
+
+authorize().then(listLabels).catch(console.error);
+
+async function test(auth){
+    const gmail = google.gmail({version: 'v1', auth})
+    await gmail.users.messages.list({
+        "maxResults": 10,
+
+        userId: 'me',
+        q: "in:Expenses is:unread",
+        includeSpamTrash: false
+    }).then(res=>res.data)
+    .then(async data => {
+      var messageList = data.messages;
+      for(var i in messageList) {
+        var message = await gmail.users.messages.get({
+          userId:'me',
+          id: messageList[i].id
+        }).then(res=>res.data)
+        var payload = Buffer.from(message.payload.parts[0].body.data, 'base64').toString().replace(/<tr>|<\/tr>|<td>|<\/td>/gm, '');
+        var amt = /(\$|SGD)\s{0,1}\d+\.\d{2}/g.exec(payload)[0] //will extract if string begins with $ or SGD and at least one digit before :.02
+        var name = /To:\s*.*/gm.exec(payload)[0].replace(/To:\s*/gm, '')//extract if string begins with To: any space in between until the next breakline
+        console.log(amt, name)
+      }
+    })
+}
+authorize().then(test).catch(console.error);
