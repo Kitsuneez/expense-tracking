ed30d05ab5176c6c80ec39d4f3a3fe8f4fd2abda
Kitsuneez <106949197+Kitsuneez@users.noreply.github.com>
2024-10-26 10:42:20 +0000

    test

--- .venv/test.py
+++ .venv/test.py
@@ -1,6 +1,10 @@
 import requests
 
-for i in range(497,0,-1):
+for i in range(20,0,-1):
+    if len(str(i))==2:
+        i = "0"+str(i)
+    elif len(str(i))==1:
+        i = "00"+str(i)
     r = requests.get(f"https://thefappening.plus/photos/e/v/evawishx-1/1000/evawishx-1_0{i}_s.jpg").content
     with open(f"images/{i}.jpg", 'wb') as handler:
         handler.write(r)
